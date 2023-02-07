154d0aa7252ca377ff4de8340f4d4a2a24cf5b59
RobinJehn <robin.jehn@gmx.de>
2023-02-07 17:09:25 +0000

    Add functional navigation

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { GiOfficeChair } from 'react-icons/gi';
 import { AiFillEye } from 'react-icons/ai';
-import { Box, Container, Row, FooterLink } from './FooterStyles';
+import { NavLink } from 'react-router-dom';
+import { Box, Container, Row } from './FooterStyles';
+import './Footer.css';
 
-import CompanyLogo from '../../assets/CompanyLogo';
+// import CompanyLogo from '../../assets/CompanyLogo';
 
 function Footer() {
   return (
@@ -13,42 +15,27 @@ function Footer() {
           {/* <FooterLink href="google.com">
             <CompanyLogo size="50px" />
           </FooterLink> */}
+          <div className="icon-wrapper">
+            <NavLink
+              style={({ isActive }) => {
+                return isActive ? { color: 'grey' } : {};
+              }}
+              to="/seat"
+            >
+              <GiOfficeChair size="inherit" />
+            </NavLink>
+          </div>
 
-          <FooterLink href="google.com">
-            <GiOfficeChair size="50px" />
-          </FooterLink>
-
-          <FooterLink href="google.com">
-            <AiFillEye size="50px" />
-          </FooterLink>
-
-          {/* <Column>
-            <Heading>About Us</Heading>
-            <FooterLink href="#">Aim</FooterLink>
-            <FooterLink href="#">Vision</FooterLink>
-            <FooterLink href="#">Testimonials</FooterLink>
-          </Column>
-          <Column>
-            <Heading>Social Media</Heading>
-            <i className="fab fa-facebook-f">
-              <span style={{ marginLeft: '10px' }}>Facebook</span>
-            </i>
-            <FooterLink href="#">
-              <i className="fab fa-instagram">
-                <span style={{ marginLeft: '10px' }}>Instagram</span>
-              </i>
-            </FooterLink>
-            <FooterLink href="#">
-              <i className="fab fa-twitter">
-                <span style={{ marginLeft: '10px' }}>Twitter</span>
-              </i>
-            </FooterLink>
-            <FooterLink href="#">
-              <i className="fab fa-youtube">
-                <span style={{ marginLeft: '10px' }}>Youtube</span>
-              </i>
-            </FooterLink>
-          </Column> */}
+          <div className="icon-wrapper">
+            <NavLink
+              style={({ isActive }) => {
+                return isActive ? { color: 'grey' } : {};
+              }}
+              to="/eye"
+            >
+              <AiFillEye size="inherit" />
+            </NavLink>
+          </div>
         </Row>
       </Container>
     </Box>
--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -2,15 +2,40 @@ import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Footer from '../components/Footer';
 
-function Hello() {
-  return <Footer />;
+function Home() {
+  return (
+    <div>
+      <div> Home </div>
+      <Footer />
+    </div>
+  );
+}
+
+function Seat() {
+  return (
+    <div>
+      <div> Seat </div>
+      <Footer />
+    </div>
+  );
+}
+
+function Eye() {
+  return (
+    <div>
+      <div> EyeTracker </div>
+      <Footer />
+    </div>
+  );
 }
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Hello />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/seat" element={<Seat />} />
+        <Route path="/eye" element={<Eye />} />
       </Routes>
     </Router>
   );
