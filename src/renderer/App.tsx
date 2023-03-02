7c0c3765ad4573b0999acf18661e4a206557616b
Ruby Imrie <s2056921@ed.ac.uk>
2023-03-02 11:10:45 +0000

    adding home page to App.tsx

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,48 +1,13 @@
-import {
-  MemoryRouter as Router,
-  Routes,
-  Route,
-  useNavigate,
-} from 'react-router-dom';
+import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import RegistrationForm from 'components/RegistrationForm';
 import EyeTrackingPage from 'components/EyeTrackingPage';
 import SettingsPage from 'components/SettingsPage';
 import Welcome from 'components/WelcomePage';
 import LoginForm from 'components/Login';
-import Footer from 'components/Footer';
 import Seat from 'components/Seat';
 import PrivateRoute from 'components/PrivateRoute';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-
-function Home() {
-  const navigate = useNavigate();
-  const forgetUser = async () => {
-    try {
-      await AsyncStorage.removeItem('loggedIn');
-      navigate('/');
-      console.log('navigation activated to /');
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  return (
-    <div>
-      <div> Home </div>
-      <form id="logout_form">
-        <button
-          form="logout_form"
-          onClick={() => forgetUser()}
-          type="submit"
-          className="logout_button"
-        >
-          Log out
-        </button>
-      </form>
-      <Footer />
-    </div>
-  );
-}
+import HomePage from 'components/HomePage';
 
 export default function App() {
   return (
@@ -53,7 +18,7 @@ export default function App() {
             path="/"
             element={
               <PrivateRoute>
-                <Home />
+                <HomePage />
                 <Welcome />
               </PrivateRoute>
             }
@@ -61,7 +26,7 @@ export default function App() {
           <Route path="/login" element={<LoginForm />} />
           <Route path="/register" element={<RegistrationForm />} />
           <Route path="/settings" element={<SettingsPage />} />
-          <Route path="/home" element={<Home />} />
+          <Route path="/home" element={<HomePage />} />
           <Route path="/seat" element={<Seat />} />
           <Route path="/eye" element={<EyeTrackingPage />} />
         </Routes>
