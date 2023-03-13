7976ea86e103952ec9dde7f1e139653b875b5cf1
Gpatch <gleb.orlov8@gmail.com>
2023-03-13 13:15:48 +0000

    Added logout button to settings

--- src/components/SettingsPage/index.tsx
+++ src/components/SettingsPage/index.tsx
@@ -1,9 +1,18 @@
+import { useNavigate } from 'react-router-dom';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import Footer from 'components/Footer';
 import DoNotDisturbForm from './DoNotDisturb';
 import NotificationForm from './Notifications';
+
 import './style.css';
 
 export default function SettingsPage() {
+  const navigate = useNavigate();
+  const forgetUser = () => {
+    AsyncStorage.removeItem('loggedIn');
+    navigate('/');
+  };
+
   return (
     <div className="Settings">
       <h1>Settings</h1>
@@ -13,6 +22,16 @@ export default function SettingsPage() {
       <div className="Form">
         <NotificationForm />
       </div>
+      <form id="logout_form">
+        <button
+          form="logout_form"
+          onClick={() => forgetUser()}
+          type="submit"
+          className="logout_button"
+        >
+          Log out
+        </button>
+      </form>
       <Footer />
     </div>
   );
