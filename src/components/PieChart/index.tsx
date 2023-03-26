bd173f952765b32de0ed9c01f1434db07a19b1aa
Ruby Imrie <s2056921@ed.ac.uk>
2023-03-26 12:52:08 +0100

    adding tooltip to piechart

--- src/components/PieChart/index.tsx
+++ src/components/PieChart/index.tsx
@@ -1,4 +1,4 @@
-import { PieChart, Pie, Legend, Label, ResponsiveContainer } from 'recharts';
+import { PieChart, Pie, Legend, Label, ResponsiveContainer, TooltipProps, Tooltip } from 'recharts';
 import './style.css';
 import { useState, ChangeEvent } from 'react';
 import { BiRefresh } from 'react-icons/bi';
@@ -21,6 +21,36 @@ export default function piechart() {
     event.preventDefault();
     console.log(`Selected Date: ${date}`);
   };
+
+  interface CustomTooltipProps extends TooltipProps {
+    active?: boolean;
+    payload?: { value: string, name: string }[];
+    label?: string;
+  }
+
+  const CustomTooltip = ({ active, payload, label }: CustomTooltipProps) => {
+    if (active && payload) {
+      const explanation = payload[0].name === "Good"
+        ? "Your posture score measures how well you maintain good posture throughout the day. A higher score means better posture."
+        : "Your posture score measures how well you maintain proper posture throughout the day. A lower score means that you should make adjustments to your posture.";
+      const totalTime = payload.reduce((sum, entry) => sum + parseInt(entry.value), 0);
+      const goodTime = parseInt(payload.find(entry => entry.name === "Good")?.value ?? "0");
+      const badTime = parseInt(payload.find(entry => entry.name === "Poor")?.value ?? "0");
+      const goodPercentage = ((goodTime / totalTime) * 100).toFixed(2);
+      const badPercentage = ((badTime / totalTime) * 100).toFixed(2);
+      const percentage = payload[0].name === "Good" ? goodPercentage : badPercentage;
+      return (
+        <div className="custom-tooltip">
+          <p>{`Posture: ${payload[0].name}`}</p>
+          <p>{`Percentage: ${percentage}`}</p>
+          <p>{explanation}</p>
+        </div>
+      );
+    }
+    return null;
+  };
+
+
   return (
     <div className="pie-chart">
       <form onSubmit={handleSubmit} className="form-container">
@@ -55,6 +85,7 @@ export default function piechart() {
             />
           </Pie>
           <Legend verticalAlign="bottom" height={36} iconType="circle" />
+          <Tooltip content={<CustomTooltip /> } />
         </PieChart>
       </ResponsiveContainer>
     </div>
